37ebe390d6856867f7e5a87cb612b08a3acc5bae
Hack Reactor Student <student@hackreactor.com>
2017-08-23 20:09:03 -0500

    passed findNRookSolution. working on findNQueensolution

--- src/solvers.js
+++ src/solvers.js
@@ -12,12 +12,28 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-
-
-
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  //instantiate a new board
+  var rookBoard = new Board({'n': n});
+  var boardSize = n;
+  var solution = rookBoard.rows(); //fixme
+  
+  //iterate through each value of row
+  for (var row = 0; row < boardSize; row++) {
+    //iterate through the column
+    for (var column = 0; column < boardSize; column++) {
+      //toggle the index on
+      rookBoard.togglePiece(row, column);
+      //check if there are row or column conflicts  
+      if (rookBoard.hasAnyColConflicts() || rookBoard.hasAnyRowConflicts()) {
+        //if true, toggle the index off
+        rookBoard.togglePiece(row, column);
+      }
+    
+    }
+  }
+      
+  
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
@@ -32,7 +48,48 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var queenBoard = new Board({'n': n});
+  var solution = queenBoard.rows(); //fixme
+  var boardSize = n;
+
+  //if board size is 2 or 3, there is no solution
+  // if (boardSize === 2 || boardSize === 3) {
+  //   return [];
+  // }
+  var inner = function (board, row, column) {
+    var toggled = [];
+    //iterate through each value of row
+    for (var row = row; row < boardSize; row++) {
+      //iterate through the column
+      for (var column = column; column < boardSize; column++) {
+        //toggle the index on
+        board.togglePiece(row, column);
+        toggled.push([row, column]);
+        //check if there are row or column conflicts  
+        if (board.hasAnyColConflicts() || board.hasAnyRowConflicts() || board.hasAnyMajorDiagonalConflicts() || board.hasAnyMinorDiagonalConflicts()) {
+          //if true, toggle the index off
+          board.togglePiece(row, column);
+          toggled.pop();
+        }
+      }
+    }
+
+    if (boardSize === toggled.length) {
+      return;
+    } else {
+      //clearing board back to zeros
+      toggled.forEach(function (item) {
+        board.togglePiece(item[0], item[1]);
+      });
+      console.log(JSON.stringify(board));
+      inner(queenBoard, row, column + 1);
+    }
+  };
+  //need helper function that adds all pieces on board
+    //call this function somewhere in recursive function to see if it equals n
+    //if it doesn't equal n, then recurse
+  //call recursive function
+  inner(queenBoard, 0, 0);
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
