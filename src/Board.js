9ddf20ce27d3cdb100505abd62b2001762fe4b4f
Hack Reactor Student <student@hackreactor.com>
2017-08-24 17:15:39 -0500

    fixed hasAnyMinorDiagonalConflicts to boardSize+4. That fixed given countNQueensSolutions

--- src/Board.js
+++ src/Board.js
@@ -181,7 +181,7 @@
       var hasDiagConflict = false;
 
       //iterate through columns
-      for (var j = 2 - boardSize; j < boardSize - 1; j++) {
+      for (var j = 1 - boardSize; j < boardSize - 1; j++) {
         //check each major diagonal at column index for conflict
         if (this.hasMajorDiagonalConflictAt(j)) {
           return true;
@@ -228,7 +228,7 @@
       var boardSize = this.get('n');
       var hasConflict = false;
       
-      for (var i = boardSize + 2; i >= 0; i--) {
+      for (var i = boardSize + 4; i >= 0; i--) {
         if (this.hasMinorDiagonalConflictAt(i)) {
           return true;
         }
